e320c145ff4645394fb1d237087182fb1ae18857
bilalelou <bilal.12elou@gmail.com>
2025-08-21 01:00:41 +0100

    feat: add a new register page placeholder

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
       },
     ],
   },
