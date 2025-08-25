fc7d4ab81b39ec16189b5f0187009afa5fe52500
bilalelou <bilal.12elou@gmail.com>
2025-08-25 19:02:48 +0100

    update file layoout app

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -305,17 +305,24 @@
             </a>
 
             <div class="logo">
-                <a href="index.html">
+                <a href="{{ route('home.index') }}">
                     <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
                 </a>
             </div>
 
-            <a href="#" class="header-tools__item header-tools__cart js-open-aside" data-aside="cartDrawer">
+            <a href="{{ route('cart.index') }}" class="header-tools__item header-tools__cart js-open-aside"
+                data-aside="cartDrawer">
                 <svg class="d-block" width="20" height="20" viewBox="0 0 20 20" fill="none"
                     xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_cart" />
                 </svg>
-                <span class="cart-amount d-block position-absolute js-cart-items-count">3</span>
+
+                {{-- This block now checks if the cart has items before showing the count --}}
+                @if (Cart::instance('cart')->count() > 0)
+                    <span class="cart-amount d-block position-absolute js-cart-items-count">
+                        {{ Cart::instance('cart')->count() }}
+                    </span>
+                @endif
             </a>
         </div>
 
@@ -354,7 +361,7 @@
                             <a href="{{ route('cart.index') }}" class="navigation__link">Cart</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="{{route('home.about')}}" class="navigation__link">About</a>
+                            <a href="{{ route('home.about') }}" class="navigation__link">About</a>
                         </li>
                         <li class="navigation__item">
                             <a href="{{ route('home.contact') }}" class="navigation__link">Contact</a>
@@ -426,7 +433,7 @@
         <div class="container">
             <div class="header-desk header-desk_type_1">
                 <div class="logo">
-                    <a href="index.html">
+                    <a href="{{ route('home.index') }}">
                         <img src="{{ asset('assets/images/logo.png') }}" alt="Uomo"
                             class="logo__image d-block" />
                     </a>
@@ -444,7 +451,7 @@
                             <a href="{{ route('cart.index') }}" class="navigation__link">Cart</a>
                         </li>
                         <li class="navigation__item">
-                            <a href="{{route('home.about')}}" class="navigation__link">About</a>
+                            <a href="{{ route('home.about') }}" class="navigation__link">About</a>
                         </li>
                         <li class="navigation__item">
                             <a href="{{ route('home.contact') }}" class="navigation__link">Contact</a>
@@ -546,9 +553,9 @@
             <div class="row row-cols-lg-5 row-cols-2">
                 <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
                     <div class="logo">
-                        <a href="index.html">
-                            <img src="{{ asset('assets/images/logo.png') }}" alt="SurfsideMedia"
-                                class="logo__image d-block" />
+                        <a href="{{ route('home.index') }}">
+                            <img src="{{ asset('assets/images/logo.png') }}"
+                                alt="{{ config('app.name', 'T9da.com') }}" class="logo__image d-block" />
                         </a>
                     </div>
                     <p class="footer-address">123 Beach Avenue, Surfside City, CA 00000</p>
@@ -603,7 +610,8 @@
                 <div class="footer-column footer-menu mb-4 mb-lg-0">
                     <h6 class="sub-menu__title text-uppercase">Company</h6>
                     <ul class="sub-menu__list list-unstyled">
-                        <li class="sub-menu__item"><a href="{{route('home.about')}}" class="menu-link menu-link_us-s">About Us</a></li>
+                        <li class="sub-menu__item"><a href="{{ route('home.about') }}"
+                                class="menu-link menu-link_us-s">About Us</a></li>
                         <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Careers</a>
                         </li>
                         <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Affiliates</a>
@@ -664,7 +672,7 @@
 
         <div class="footer-bottom">
             <div class="container d-md-flex align-items-center">
-                <span class="footer-copyright me-auto">©2025  T9da.com</span>
+                <span class="footer-copyright me-auto">©2025 T9da.com</span>
                 <div class="footer-settings d-md-flex align-items-center">
                     <a href="privacy-policy.html">Privacy Policy</a> &nbsp;|&nbsp; <a
                         href="terms-conditions.html">Terms &amp;
@@ -678,7 +686,8 @@
     <footer class="footer-mobile container w-100 px-5 d-md-none bg-body">
         <div class="row text-center">
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('home.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_home" />
@@ -688,7 +697,8 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                <a href="{{ route('shop.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <svg class="d-block" width="18" height="18" viewBox="0 0 18 18" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <use href="#icon_hanger" />
@@ -698,13 +708,20 @@
             </div>
 
             <div class="col-4">
-                <a href="index.html" class="footer-mobile__link d-flex flex-column align-items-center">
+                {{-- The link now points to the correct wishlist route --}}
+                <a href="{{ route('wishlist.index') }}"
+                    class="footer-mobile__link d-flex flex-column align-items-center">
                     <div class="position-relative">
                         <svg class="d-block" width="18" height="18" viewBox="0 0 20 20" fill="none"
                             xmlns="http://www.w3.org/2000/svg">
                             <use href="#icon_heart" />
                         </svg>
-                        <span class="wishlist-amount d-block position-absolute js-wishlist-count">3</span>
+
+                        @if (Cart::instance('wishlist')->content()->count() > 0)
+                            <span class="wishlist-amount d-block position-absolute js-wishlist-count">
+                                {{ Cart::instance('wishlist')->content()->count() }}
+                            </span>
+                        @endif
                     </div>
                     <span>Wishlist</span>
                 </a>
