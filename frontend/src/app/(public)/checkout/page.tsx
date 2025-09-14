227d2e8782900d14352eec0207641e54a076e36e
bilalelou <bilal.12elou@gmail.com>
2025-09-14 18:35:31 +0100

    fix problem

--- frontend/src/app/(public)/checkout/page.tsx
+++ frontend/src/app/(public)/checkout/page.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+export const dynamic = 'force-dynamic';
+
 
 // Define interfaces
 interface CartItem {
